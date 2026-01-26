48e5ac028aa6a3ee8a17309e57a2bdf1114aa353
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-01-25 20:22:59 -0800

    add properties to CreateAuditLogRequest record

--- Dtos/CreateAuditLogRequest.cs
+++ Dtos/CreateAuditLogRequest.cs
@@ -1,3 +1,9 @@
 namespace AuditLogs.Api.Dtos;
 
-public record CreateAuditLogRequest();
+public record CreateAuditLogRequest(
+    string Action,
+    string Entity,
+    string? EntityId,
+    string PerformedBy,
+    string? MetadataJson
+    );
