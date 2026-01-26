00509799bd883faedec3429123182a8d043cbf8c
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-01-25 19:51:29 -0800

    add properties to AuditLogEntry model

--- Models/AuditLogEntry.cs
+++ Models/AuditLogEntry.cs
@@ -2,5 +2,11 @@ namespace AuditLogs.Api.Models;
 
 public class AuditLogEntry
 {
-    
+    public long Id { get; set; }
+    public string Action { get; set; } = string.Empty;
+    public string Entity { get; set; } = string.Empty;
+    public string? EntityId { get; set; } = string.Empty;
+    public string PerformedBy { get; set; } = string.Empty;
+    public DateTime OccuredOnUtc { get; set; } =  DateTime.UtcNow;
+    public string? MetadataJson { get; set; } = string.Empty;
 }
