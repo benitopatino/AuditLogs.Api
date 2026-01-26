057cd3623a339c1e1f08c0f5ee8a67a8579ffca9
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-01-25 20:25:58 -0800

    init AuditLogsController template

---
+++ Controllers/AuditLogsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuditLogs.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditLogsController : ControllerBase
+    {
+    }
+}
