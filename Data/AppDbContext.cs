cced55f78c5e9116b55b15953f456eec7664a1d8
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-01-25 20:06:46 -0800

    complete AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,33 @@
+using AuditLogs.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuditLogs.Api.Data;
 
-public class AppDbContext
+public class AppDbContext :DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<AuditLogEntry> AuditLogEntries => Set<AuditLogEntry>();
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AuditLogEntry>(entity =>
+        {
+            entity.Property(x => x.Action)
+                .HasMaxLength(100)
+                .IsRequired();
+            
+            entity.Property(x => x.Entity)
+                .HasMaxLength(100)
+                .IsRequired();
+            
+            entity.Property(x => x.PerformedBy)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            entity.HasIndex(x => x.OccuredOnUtc);
+        });
+    }
 }
