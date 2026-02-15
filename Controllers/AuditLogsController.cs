173d076a9ccb93671e4e30373dee94534d12dccf
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-02-14 16:14:54 -0800

    add create audit log endpoint

--- Controllers/AuditLogsController.cs
+++ Controllers/AuditLogsController.cs
@@ -1,4 +1,6 @@
 using AuditLogs.Api.Data;
+using AuditLogs.Api.Dtos;
+using AuditLogs.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,7 +16,30 @@ namespace AuditLogs.Api.Controllers
         {
             _dbContext = dbContext;
         }
-        
-        
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAuditLogRequest request)
+        {
+            try
+            {
+                var entry = new AuditLogEntry()
+                {
+                    Action = request.Action,
+                    Entity = request.Entity,
+                    EntityId = request.EntityId,
+                    PerformedBy = request.PerformedBy,
+                    MetadataJson = request.MetadataJson,
+                };
+            
+                _dbContext.AuditLogEntries.Add(entry);
+                await _dbContext.SaveChangesAsync();
+                return Ok(entry.Entity);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request. :(");
+            }
+
+        }
     }
 }
